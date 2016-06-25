f01e6d0f59a78825375c0b13a6c1a33b00cdbbca
eSilence <eSilence@yandex.ru>
2016-06-25 22:33:11 +0300

    add BaseActivity, progress_splash

--- app/src/main/java/com/softdesign/devintensive/ui/activities/MainActivity.java
+++ app/src/main/java/com/softdesign/devintensive/ui/activities/MainActivity.java
@@ -3,13 +3,16 @@ package com.softdesign.devintensive.ui.activities;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
 
 import com.softdesign.devintensive.R;
 import com.softdesign.devintensive.utils.ConstantManager;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends BaseActivity implements View.OnClickListener {
 
     private static final String TAG = ConstantManager.TAG_PREFIX + "MainActivity";
+    private ImageView mCallImg;
 
 
     @Override
@@ -18,6 +21,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Log.d(TAG, "onCreate");
 
+        mCallImg = (ImageView)findViewById(R.id.call_img);
+        mCallImg.setOnClickListener(this);
       //  if (savedInstanceState == null) {
             //активити запущено впервые
 
@@ -68,5 +73,14 @@ public class MainActivity extends AppCompatActivity {
         super.onSaveInstanceState(outState);
         Log.d(TAG, "onSaveInstanceState");
 
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.call_img:
+                showProgress();
+                break;
+        }
     }
 }
